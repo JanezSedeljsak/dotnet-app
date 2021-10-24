f335abb5a644bcceb051534f38a1f74d4bebd4fe
Janez Sedeljsak <janez.sedeljsak@gmail.com>
2021-10-24 22:29:45 +0200

    prepare model lists for country and region

--- Services/Helpers.cs
+++ Services/Helpers.cs
@@ -1,6 +1,7 @@
-namespace Services.Helpers;
 using Newtonsoft.Json;
+using Models;
 
+namespace Services.Helpers;
 public class Neki {}
 
 public class Helpers {
@@ -8,10 +9,34 @@ public class Helpers {
         var client = new HttpClient();
         var data = await client.GetStringAsync("https://restcountries.com/v3.1/all");
         var response = JsonConvert.DeserializeObject<dynamic>(data);
+
+        var countries = new List<Tuple<string, Country>>();
+        var regions = new Dictionary<string, Region>();
+
         foreach (var country in response) {
-            Console.WriteLine(country);
+            string region = country["region"];
+            string countryName = country["name"]["common"];
+            regions[region] = new Region {
+                name = region
+            };
+
+            countries.Add(Tuple.Create(region, new Country {
+                name = countryName
+            }));
         }
-        //Console.WriteLine(obj.ToString());
+
+        var countryData = new List<Country>();
+        foreach (var country in countries) {
+            var tempCountry = country.Item2;
+            if (regions.TryGetValue(country.Item1, out var tempRegion)) {
+                tempCountry.region = tempRegion;
+            }
+
+            countryData.Add(tempCountry);
+        }
+
+        var regionsData = regions.Values.ToList();
+
         return true;
     }
 }
